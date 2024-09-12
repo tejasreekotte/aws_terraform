a8925de52ae376c2ec8e24ab15b3f341063d6765
tejasreekotte <33227563+tejasreekotte@users.noreply.github.com>
2024-09-12 20:28:54 +0530

    changes added

--- configure-named-profile.sh
+++ configure-named-profile.sh
@@ -4,9 +4,9 @@
 set -eu
 
 # configure named profile
-aws configure set aws_access_key_id $aws_access_key --profile $PROFILE_NAME
-aws configure set aws_secret_access_key $aws_secret_key --profile $PROFILE_NAME
-aws configure set region $aws_region --profile $PROFILE_NAME
+aws configure set aws_access_key_id $TF_VAR_aws_access_key --profile $PROFILE_NAME
+aws configure set aws_secret_access_key $TF_VAR_aws_secret_key --profile $PROFILE_NAME
+aws configure set region $TF_VAR_aws_region --profile $PROFILE_NAME
 
 # verify that profile is configured
 aws configure list --profile $PROFILE_NAME
